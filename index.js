b9c1a8868dd3e06e20c36729d47098852a857c51
Andreas Kollmann <e12125444@student.tuwien.ac.at>
2025-11-03 20:25:46 +0100

    set up node with d3 and duckdb, import assignment1

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { DuckDBInstance } from '@duckdb/node-api';
+
+
+const app = express();
+const port = 3000;
+
+const db = await DuckDBInstance.create('resources/openpowerlifting.ddb');
+const conn = await db.connect();
+console.log("connected to db")
+
+app.use(express.static("public"));
+
+app.get("/assignment0", (req, res) => {
+  const query = `
+      select tested, max(Dots), avg(Dots), median(Dots), max(TotalKg), avg(TotalKg), median(TotalKg), max(Best3SquatKg) as Squat, max(Best3BenchKg) as Bench, max(Best3DeadliftKg) as Deadlift
+      from lifts
+      where sanctioned = true and Dots > 400
+      group by tested;
+  `;
+
+  const result = conn.runAndReadAll(query).then(
+    (result) => {
+      res.json(result.getColumnsObject())
+    }
+  );
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
