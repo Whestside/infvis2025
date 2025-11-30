5e050c5f8041d947d0cf15bc2700b976ace7a7e5
Andreas Kollmann <e12125444@student.tuwien.ac.at>
2025-11-30 16:00:18 +0100

    individual assignment 2

--- index.js
+++ index.js
@@ -13,9 +13,24 @@ app.use(express.static("public"));
 
 app.get("/assignment0", (req, res) => {
   const query = `
-      select tested, max(Dots), avg(Dots), median(Dots), max(TotalKg), avg(TotalKg), median(TotalKg), max(Best3SquatKg) as "Best Squat", max(Best3BenchKg) as "Best Bench", max(Best3DeadliftKg) as "Best Deadlift"
+      select tested, max(Best3SquatKg) as "Best Squat", max(Best3BenchKg) as "Best Bench", max(Best3DeadliftKg) as "Best Deadlift"
       from lifts
-      where sanctioned = true and Dots > 400 and Dots < 550
+      where sanctioned = true and Dots > 400
+      group by tested;
+  `;
+
+  const result = conn.runAndReadAll(query).then(
+    (result) => {
+      res.json(result.getColumnsObject())
+    }
+  );
+
+});
+
+app.get("/assignment1", (req, res) => {
+  const query = `
+      select tested, avg(best3benchkg) as "Average Bench", max(Best3BenchKg) as "Best Bench", median(Best3BenchKg) as "Median Bench"
+      from lifts
       group by tested;
   `;
 
